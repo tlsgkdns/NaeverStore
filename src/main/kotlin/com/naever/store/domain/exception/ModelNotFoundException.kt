7078b2af62a8e2aa8ecc269b7ccb96321aa81870
umin <adorecamus@gmail.com>
2024-01-23 12:12:07 +0900

    Feat: 특정 모델 없는 경우 예외 처리

---
+++ src/main/kotlin/com/naever/store/domain/exception/ModelNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.naever.store.domain.exception
+
+data class ModelNotFoundException(val modelName: String, val id: Long?): RuntimeException(
+    "Model $modelName not found with given id: $id"
+)
