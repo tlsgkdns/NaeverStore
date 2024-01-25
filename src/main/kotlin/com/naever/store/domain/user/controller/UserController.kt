59709b3e0b4001600a08a10bb189796ae51c6614
tlsgkdns <tlsgkdns@gmail.com>
2024-01-25 11:31:34 +0900

    Validation 추가

--- src/main/kotlin/com/naever/store/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/naever/store/domain/user/controller/UserController.kt
@@ -2,9 +2,11 @@ package com.naever.store.domain.user.controller
 
 import com.naever.store.domain.user.dto.*
 import com.naever.store.domain.user.service.UserService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,8 +15,11 @@ class UserController(
     private val userService: UserService
 ) {
     @PostMapping()
-    fun signUp(@RequestBody userRegisterRequest: UserRegisterRequest): ResponseEntity<UserResponse>
+    fun signUp(@RequestBody @Valid userRegisterRequest: UserRegisterRequest,
+        bindingResult: BindingResult): ResponseEntity<UserResponse>
     {
+        if(bindingResult.hasErrors())
+            throw IllegalArgumentException(bindingResult.fieldError?.defaultMessage ?: "")
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(userService.registerUser(userRegisterRequest))
     }
@@ -26,17 +31,23 @@ class UserController(
     }
     @PreAuthorize("hasAuthority('ROLE_USER') && authentication?.principal?.id == #userId")
     @PutMapping("/{userId}")
-    fun updateUser(@PathVariable userId: Long, updateRequest: UserUpdateRequest)
+    fun updateUser(@PathVariable userId: Long, @Valid updateRequest: UserUpdateRequest
+                   , bindingResult: BindingResult)
     : ResponseEntity<UserResponse>
     {
+        if(bindingResult.hasErrors())
+            throw IllegalArgumentException(bindingResult.fieldError?.defaultMessage ?: "")
         return ResponseEntity.status(HttpStatus.OK)
             .body(userService.updateUser(userId, updateRequest))
     }
     @PreAuthorize("hasAuthority('ROLE_USER') && authentication?.principal?.id == #userId")
     @PatchMapping("/{userId}")
     fun updatePassword(@PathVariable userId: Long,
-                       @RequestBody userPasswordUpdateDTO: UserPasswordUpdateRequest): ResponseEntity<UserResponse>
+                       @RequestBody @Valid userPasswordUpdateDTO: UserPasswordUpdateRequest,
+                       bindingResult: BindingResult): ResponseEntity<UserResponse>
     {
+        if(bindingResult.hasErrors())
+            throw IllegalArgumentException(bindingResult.fieldError?.defaultMessage ?: "")
         return ResponseEntity.status(HttpStatus.OK)
             .body(userService.updatePassword(userId, userPasswordUpdateDTO))
     }
