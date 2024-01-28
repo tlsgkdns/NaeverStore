0aaac6d883a6eee468d8f4a367b5ce2a90647123
umin <adorecamus@gmail.com>
2024-01-29 01:15:34 +0900

    Refactor: UserResponse 에 닉네임 추가

--- src/main/kotlin/com/naever/store/domain/user/dto/UserResponse.kt
+++ src/main/kotlin/com/naever/store/domain/user/dto/UserResponse.kt
@@ -6,6 +6,7 @@ import com.naever.store.domain.user.model.UserType
 data class UserResponse(
     val id: Long,
     val email: String,
+    val nickname: String,
     val address: String,
     val introduction: String,
     val authority: UserType
@@ -18,6 +19,7 @@ data class UserResponse(
             return UserResponse(
                 user.id!!,
                 user.email,
+                user.nickname,
                 user.address,
                 user.introduction,
                 user.authority
